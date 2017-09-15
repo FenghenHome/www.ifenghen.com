2e11d8863acf6f6ccef1e6036d26942e0e5f16b0
FenghenHome <s.sunfeihu@gmail.com>
2017-09-15 14:55:45 +0800

    update node_qr.php
    Signed-off-by: FenghenHome <s.sunfeihu@gmail.com>

--- user/node_qr.php
+++ user/node_qr.php
@@ -17,24 +17,35 @@ $url = \Ss\User\Comm::base64_url_encode1_url($ssurl);
 $ssqr = "ssr://".$url;
 ?>
 
-<p align="center"><a>Android 客户端:</a><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/Shadowsocksr-android-3.4.0.6.apk" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>Windows 客户端:</a><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/Shadowsocksr-csharp-4.7.0.zip" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>iOS 客户端:</a><a href="https://www.25pp.com/ios/detail_1923429" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>Mac OS X 客户端:</a><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/ShadowsocksX-NG-R8.dmg" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>ShadowsocksR-libev for OpenWrt客户端:</a><a href="https://github.com/ywb94/openwrt-ssr/releases" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>avege for 多平台客户端:</a><a href="https://github.com/avege/avege/releases" rel="nofollow" target="_blank">下载</a>
-<p align="center"><a>electron-ssr for 多平台客户端:</a><a href="https://github.com/erguotou520/electron-ssr/releases" rel="nofollow" target="_blank">下载</a>
-
 <div align="center">
     <div id="qrcode"></div>
 </div>
-<p align="center"><a>服务器:</a> <code><?php echo $server; ?></code></p>
-<p align="center"><a>远程端口:</a> <code><?php echo $port; ?></code></p>
-<p align="center"><a>密码:</a> <code><?php echo $pass; ?></code></p>
-<p align="center"><a>加密方法:</a> <code><?php echo $method; ?></code></p>
-<p align="center"><a>协议:</a> <code><?php echo $protocol; ?></code></p>
-<p align="center"><a>混淆方式:</a> <code><?php echo $obfs; ?></code></p>
-<p align="center"><a>混淆参数:</a> <code>cloudflare.com</code></p>
+
+<p align="center">
+<table border="1" style=‘margin：0px auto’>
+<tr><td>服务器:</td><td><?php echo $server; ?></td></tr>
+<tr><td>远程端口:</td><td><?php echo $port; ?></td></tr>
+<tr><td>密码:</td><td><?php echo $pass; ?></td></tr>
+<tr><td>加密方法:</td><td><?php echo $method; ?></td></tr>
+<tr><td>协议:</td><td><?php echo $protocol; ?></td></tr>
+<tr><td>混淆方式:</td><td><?php echo $obfs; ?></td></tr>
+<tr><td>混淆参数:</td><td>cloudflare.com</td></tr>
+</table>
+</p>
+</p>
+
+<p valign="middle">
+<p align="center">
+<table border="1" style=‘margin：0px auto’>
+<tr><td>Android 客户端:</td><td><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/Shadowsocksr-android-3.4.0.6.apk" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>Windows 客户端:</td><td><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/Shadowsocksr-csharp-4.7.0.zip" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>iOS 客户端:</td><td><a href="https://www.25pp.com/ios/detail_1923429" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>Mac OS X 客户端:</td><td><a href="https://github.com/FenghenHome/shadowsocksr-download/raw/master/ShadowsocksX-NG-R8.dmg" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>OpenWrt客户端:</td><td><a href="https://github.com/ywb94/openwrt-ssr/releases" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>avege 多平台客户端:</td><td><a href="https://github.com/avege/avege/releases" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>electron-ssr 多平台客户端:</td><td><a href="https://github.com/erguotou520/electron-ssr/releases" rel="nofollow" target="_blank">下载</a></td></tr>
+</table>
+</p>
 
 <script src="https://cdn.staticfile.org/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdn.staticfile.org/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
