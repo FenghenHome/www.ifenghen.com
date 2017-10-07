003d9d51d5ee271ed9c563c849844bad38856074
FenghenHome <s.sunfeihu@gmail.com>
2017-10-08 00:20:27 +0800

    update node_qr.php
    Signed-off-by: FenghenHome <s.sunfeihu@gmail.com>

--- user/node_qr.php
+++ user/node_qr.php
@@ -37,14 +37,15 @@ $ssqr = "ssr://".$url;
 <p valign="middle">
 <p align="center">
 <table border="1" style=‘margin：0px auto’>
-<tr><td>Android 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/Shadowsocksr-android-3.4.0.6.apk" rel="nofollow" target="_blank">下载</a></td></tr>
-<tr><td>Windows 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/Shadowsocksr-csharp-4.7.0.zip" rel="nofollow" target="_blank">下载</a></td></tr>
-<tr><td>iOS 客户端</td><td><a href="https://www.25pp.com/ios/detail_1923429" rel="nofollow" target="_blank">下载</a></td></tr>
-<tr><td>Mac OS X 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/ShadowsocksX-NG-R8.dmg" rel="nofollow" target="_blank">下载</a></td></tr>
-<tr><td>OpenWrt客户端</td><td><a href="https://github.com/ywb94/openwrt-ssr/releases" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>Android 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/Shadowsocksr-android-3.4.0.6.apk" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8efead858d.png" rel="nofollow" target="_blank">教程</a></td></tr>
+<tr><td>Windows 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/Shadowsocksr-csharp-4.7.0.zip" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8f73bd160f.png" rel="nofollow" target="_blank">教程</a></td></tr>
+<tr><td>Windows 游戏客户端</td><td><a href="https://www.sockscap64.com/sstap/" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8ef915181c.png" rel="nofollow" target="_blank">教程</a></td></tr>
+<tr><td>iOS 客户端</td><td><a href="https://www.25pp.com/ios/detail_1923429" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8f3cda6710.png" rel="nofollow" target="_blank">教程</a></td></tr>
+<tr><td>Mac OS X 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/ShadowsocksX-NG-R8.dmg" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8efe79a9cb.png" rel="nofollow" target="_blank">教程</a></td></tr>
+<tr><td>OpenWrt 客户端</td><td><a href="https://github.com/FenghenHome/breakwa11.github.io/raw/master/download/luci-app-shadowsocksR-GFW_1.2.5_all.ipk" rel="nofollow" target="_blank">下载</a></td></tr>
 <tr><td>avege 多平台客户端</td><td><a href="https://github.com/avege/avege/releases" rel="nofollow" target="_blank">下载</a></td></tr>
 <tr><td>electron-ssr 多平台客户端</td><td><a href="https://github.com/erguotou520/electron-ssr/releases" rel="nofollow" target="_blank">下载</a></td></tr>
-<tr><td>Shadowrocket IPA Cracked</td><td><a href="https://www.iphonecake.com/app_932747118_.html" rel="nofollow" target="_blank">下载</a></td></tr>
+<tr><td>Shadowrocket IPA Cracked</td><td><a href="https://www.iphonecake.com/app_932747118_.html" rel="nofollow" target="_blank">下载</a></td><td><a href="https://i.loli.net/2017/10/07/59d8f3cda6710.png" rel="nofollow" target="_blank">教程</a></td></tr>
 <tr><td>A.BIG.T IPA Cracked</td><td><a href="https://www.iphonecake.com/app_1051326718_.html" rel="nofollow" target="_blank">下载</a></td></tr>
 </table>
 </p>
